733171a565ef99fa715b2b6e259ff7c15f04e501
vitalii43 <2kn15b.bondarchuk@gmail.com>
2019-10-20 01:47:59 +0300

    add different marker

--- src/components/shared/GlobeContainer.js
+++ src/components/shared/GlobeContainer.js
@@ -3,6 +3,7 @@ import Globe from './Globe';
 import style from './style.scss';
 import 'worldwindjs'; // WorldWind
 import markerIcon from '../../../assets/icons8-marker-48.png'
+import like from '../../../assets/like.png'
 
 const createLayer = () => {
     const placemarkLayer = new WorldWind.RenderableLayer("Placemark");
@@ -11,43 +12,34 @@ const createLayer = () => {
     return placemarkLayer;
 }
 
-const createPlacemark = ({label, lt, lg ,alt}) => {
+const createPlacemark = ({label, lt, lg, alt, markerType}) => {
     const placemarkAttributes = new WorldWind.PlacemarkAttributes(null);
 
     placemarkAttributes.imageOffset = new WorldWind.Offset(
         WorldWind.OFFSET_FRACTION, 0.3,
-        WorldWind.OFFSET_FRACTION, 0.0);
-    
+        WorldWind.OFFSET_FRACTION, 0.0);    
     placemarkAttributes.labelAttributes.color = WorldWind.Color.YELLOW;
     placemarkAttributes.labelAttributes.offset = new WorldWind.Offset(
                 WorldWind.OFFSET_FRACTION, 0.5,
                 WorldWind.OFFSET_FRACTION, 1.0);
-
-    placemarkAttributes.imageSource = markerIcon;
     
+    placemarkAttributes.imageSource = markerType === 'events'? markerIcon : like;
+
     const position = new WorldWind.Position(lt, lg, alt);
     const placemark = new WorldWind.Placemark(position, false, placemarkAttributes);
-    
     placemark.label = `
         ${label}\n
         Lat ${placemark.position.latitude.toPrecision(4).toString()}, Lon ${placemark.position.longitude.toPrecision(5).toString()}
     `;
-    placemark.alwaysOnTop = true;    
-    console.log(placemark)
-    var clickRecognizer = new WorldWind.ClickRecognizer(placemark, 
-        function(recognizer) {
-            
-    });
-    console.log(clickRecognizer)
-
+    placemark.alwaysOnTop = true;
     return placemark;
 }
 
 const mock = [
-    { label: 'label1', lt: 55, lg: -106, alt: 100},
-    { label: 'label2', lt: 15, lg: -106, alt: 100},
-    { label: 'label3', lt: 25, lg: -106, alt: 100},
-    { label: 'label4', lt: 75, lg: -106, alt: 100},
+    { label: 'label1', lt: 55, lg: -106, alt: 100, markerType: 'people'},
+    { label: 'label2', lt: 15, lg: -106, alt: 100, markerType: 'events'},
+    { label: 'label3', lt: 25, lg: -106, alt: 100, },
+    { label: 'label4', lt: 75, lg: -106, alt: 100, },
 ]
 
 export default class ClobeContainer extends React.Component{
