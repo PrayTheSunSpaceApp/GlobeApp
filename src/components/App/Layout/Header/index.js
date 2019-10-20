6a9308a7cc44ca132ef39534258407d9fdaa1286
umbrra <artes287@gmail.com>
2019-10-20 09:33:43 +0300

    header links fix

--- src/components/App/Layout/Header/index.js
+++ src/components/App/Layout/Header/index.js
@@ -6,17 +6,21 @@ const Header = () => {
   return (
     <header className={style.container}>
       <div className={style.brandContainer}>
-        <div className={style.logoContainer}>
-          <img src={logo} className={style.headerImg} alt="brand" />
-        </div>
+        <Link to="/" className={style.headerLink}>
+          <div className={style.logoContainer}>
+            <img src={logo} className={style.headerImg} alt="brand" />
+          </div>
+        </Link>
         <div class={style.mainHeader}>
           <div className={style.titleContainer}>
-            <span className={style.headerTextSpan}>
-              <span className={style.headerGreen}>Connect</span> 
-              {" "}to{" "}
-              <span className={style.headerBlue}>life</span>
-            </span>
-            <p className={style.headerText}>Find, Discuse, Share</p>
+            <Link to="/" className={style.headerLink}>
+              <span className={style.headerTextSpan}>
+                <span className={style.headerGreen}>Connect</span> 
+                {" "}to{" "}
+                <span className={style.headerBlue}>life</span>
+              </span>
+              <p className={style.headerText}>Find, Discuse, Share</p>
+            </Link>
           </div>
           <div className={style.headerNavGroup}>
             <Link to="/events" className={style.headerNav}>
@@ -25,7 +29,7 @@ const Header = () => {
             <Link to="/events" className={style.headerNav}>
               Near
             </Link>
-            <Link to="/events" className={style.headerLogin}>
+            <Link to="/signin" className={style.headerLogin}>
               Profile
             </Link>
           </div>
